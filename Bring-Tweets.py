2f547b47287d069235df256be277fd1975f5a27f
dongpilYu <yoodongphil@naver.com>
2018-05-31 17:28:17 +0900

    modify Bring-Tweets.py

--- Bring-Tweets.py
+++ Bring-Tweets.py
@@ -2,13 +2,19 @@ from pymongo import MongoClient
 import json
 from bson.json_util import dumps
 from bson.objectid import ObjectId
+import re
 
 client = MongoClient("localhost",27017)
 db = client.Twitchu
 UserTweets = db.UserTweets
 Users = db.Users
+p = re.compile(r'https?://.+', re.DOTALL)
+g = re.compile(r'@\S+\s')
+chinese = re.compile(u'[⺀-⺙⺛-⻳⼀-⿕々〇〡-〩〸-〺〻㐀-䶵一-鿃豈-鶴侮-頻並-龎]', re.UNICODE)
+japanese = re.compile(u'[\u3000-\u303f\u3040-\u309f\u30a0-\u30ff\uff00-\uff9f\u4e00-\u9faf\u3400-\u4dbf]', re.UNICODE)
 
 UserCursor = Users.find({})
+
 for UserDocument in UserCursor:
     dict = dumps(UserDocument)
     dict = json.loads(dict)
@@ -18,20 +24,19 @@ for UserDocument in UserCursor:
     for UserTweetsDocument in UserTweetsCursor:
         dict = dumps(UserTweetsDocument)
         dict = json.loads(dict)
-        print(dict['tweet'])
+        httpText = p.search(dict["tweet"])
+        atText = g.search(dict["tweet"])
 
+        if httpText:
+            dict['tweet'] = dict['tweet'].replace(httpText.group(),'')
 
+        if atText:
+            dict['tweet'] = dict['tweet'].replace(atText.group(),'')
+
+        text = re.sub(chinese, '', dict['tweet'])
+        text = re.sub(japanese, '', dict['tweet'])
+
+        print(text)
 
 #TODO
-"""
-사용자 목록을 뒤지고, 하나씩 가져온다. 
-리트윗된 것을 가져오고, 트윗된 것을 가져온다.
 
-"""
-
-"""
-schema를 디자인할 때 고려사항
-- 사용자 요구에 따라 디자인
-- 객체들을 함께 사용하게 되면 한 document에 합쳐서 사용
-- 데이터를 작성할 때 join
-"""
